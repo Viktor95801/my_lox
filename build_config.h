c1b4d82a263a6001e6908ec9bce1f9f581f957fa
viktor95801 <155126585+Viktor95801@users.noreply.github.com>
2025-03-07 22:11:00 -0300

    Initial setup

---
+++ build_config.h
@@ -0,0 +1,17 @@
+#ifndef BUILD_CONFIG_H
+#define BUILD_CONFIG_H
+
+#define DEBUG 0
+#define RELEASE 0
+#define DEV 1
+#define EXTRA_WARNING 1
+
+#define COMPILER "gcc"
+
+#define BUILD_FOULDER "build/"
+#define APP_NAME "lox"
+#define BIN_PATH BUILD_FOULDER"bin/"
+#define SRC_FOULDER "src/"
+#define INCLUDE_FOULDER "includes"
+
+#endif
